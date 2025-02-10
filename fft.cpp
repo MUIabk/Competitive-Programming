923b6e03df6afb9a473d6781e4231bba1893135a
Aayan <128874715+MUIabk@users.noreply.github.com>
2025-02-11 01:11:32 +0530

    Update fft.cpp

--- fft.cpp
+++ fft.cpp
@@ -1,55 +1,60 @@
 typedef complex<double> C;
 typedef vector<double> vd;
-void fft(vector<C>& a) {
-	int n = sz(a), L = 31 - __builtin_clz(n);
-	static vector<complex<long double>> R(2, 1);
-	static vector<C> rt(2, 1);  // (^ 10% faster if double)
-	for (static int k = 2; k < n; k *= 2) {
-		R.resize(n); rt.resize(n);
-		auto x = polar(1.0L, acos(-1.0L) / k);
-		FOR(i,k,2*k) rt[i] = R[i] = i&1 ? R[i/2] * x : R[i/2];
-	}
-	vi rev(n);
-	FOR(i,0,n) rev[i] = (rev[i / 2] | (i & 1) << L) / 2;
-	FOR(i,0,n) if (i < rev[i]) swap(a[i], a[rev[i]]);
-	for (int k = 1; k < n; k *= 2)
-		for (int i = 0; i < n; i += 2 * k) FOR(j,0,k) {
-			// C z = rt[j+k] * a[i+j+k]; // (25% faster if hand-rolled)  /// include-line
-			auto x = (double *)&rt[j+k], y = (double *)&a[i+j+k];        /// exclude-line
-			C z(x[0]*y[0] - x[1]*y[1], x[0]*y[1] + x[1]*y[0]);           /// exclude-line
-			a[i + j + k] = a[i + j] - z;
-			a[i + j] += z;
-		}
+void fft(vector<complex<double>> &a, bool inv){
+    int n=a.size();
+    for(int i=1,j=0;i<n;i++){
+        int b=n>>1;
+        for(; j&b; b>>=1) j-=b;
+        j+=b;
+        if(i<j) swap(a[i],a[j]);
+    }
+    for(int l=2;l<=n;l<<=1){
+        double ang=2*M_PI/l*(inv?-1:1);
+        complex<double> wlen(cos(ang),sin(ang));
+        for(int i=0;i<n;i+=l){
+            complex<double> w(1);
+            for(int j=0;j<l/2;j++){
+                complex<double> u=a[i+j], v=a[i+j+l/2]*w;
+                a[i+j]=u+v;
+                a[i+j+l/2]=u-v;
+                w*=wlen;
+            }
+        }
+    }
+    if(inv) for(auto &x:a) x/=n;
 }
-vd conv(const vd& a, const vd& b) {
-	if (a.empty() || b.empty()) return {};
-	vd res(sz(a) + sz(b) - 1);
-	int L = 32 - __builtin_clz(sz(res)), n = 1 << L;
-	vector<C> in(n), out(n);
-	copy(all(a), begin(in));
-	FOR(i,0,sz(b)) in[i].imag(b[i]);
-	fft(in);
-	for (C& x : in) x *= x;
-	FOR(i,0,n) out[i] = in[-i & (n - 1)] - conj(in[i]);
-	fft(out);
-	FOR(i,0,sz(res)) res[i] = imag(out[i]) / (4 * n);
-	return res;
+ 
+vector<double> conv(const vector<double> &a, const vector<double> &b){
+    int n=1;
+    while(n < a.size()+b.size()-1) n<<=1;
+    vector<complex<double>> fa(n), fb(n);
+    for(int i=0;i<a.size();i++) fa[i]=a[i];
+    for(int i=a.size();i<n;i++) fa[i]=0;
+    for(int i=0;i<b.size();i++) fb[i]=b[i];
+    for(int i=b.size();i<n;i++) fb[i]=0;
+    fft(fa,false);
+    fft(fb,false);
+    for(int i=0;i<n;i++) fa[i]*=fb[i];
+    fft(fa,true);
+    vector<double> r(n);
+    for(int i=0;i<n;i++) r[i]=round(fa[i].real());
+    return r;
 }
 typedef vector<ll> vl;
 template<int M> vl convMod(const vl &a, const vl &b) {
 	if (a.empty() || b.empty()) return {};
 	vl res(sz(a) + sz(b) - 1);
-	int B=32-__builtin_clz(sz(res)), n=1<<B, cut=int(sqrt(M));
+	int B=32-__builtin_clz(sz(res)), n=1<<B, cut=ll(sqrt(M));
 	vector<C> L(n), R(n), outs(n), outl(n);
-	FOR(i,0,sz(a)) L[i] = C((int)a[i] / cut, (int)a[i] % cut);
-	FOR(i,0,sz(b)) R[i] = C((int)b[i] / cut, (int)b[i] % cut);
-	fft(L), fft(R);
+	FOR(i,0,sz(a)) L[i] = C((ll)a[i] / cut, (ll)a[i] % cut);
+	FOR(i,0,sz(b)) R[i] = C((ll)b[i] / cut, (ll)b[i] % cut);
+	fft(L, false), fft(R, false);
 	FOR(i,0,n) {
 		int j = -i & (n - 1);
 		outl[j] = (L[i] + conj(L[j])) * R[i] / (2.0 * n);
 		outs[j] = (L[i] - conj(L[j])) * R[i] / (2.0 * n) / 1i;
 	}
-	fft(outl), fft(outs);
+	fft(outl, false), fft(outs, false);
 	FOR(i,0,sz(res)) {
 		ll av = ll(real(outl[i])+.5), cv = ll(imag(outs[i])+.5);
 		ll bv = ll(imag(outl[i])+.5) + ll(real(outs[i])+.5);
